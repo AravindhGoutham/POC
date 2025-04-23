acf3cbc86898b6e469f88edac2ed32598c0ee99b
AravindhGoutham <aggoutham55@gmail.com>
2025-04-23 13:30:26 -0600

    POC

--- getconfig.py
+++ getconfig.py
@@ -9,7 +9,6 @@ def configurations():
 
     with open("SSHinfo.json", "r") as f:
         ssh_info = json.load(f)
-
     routers = []
     for entry in ssh_info:
         routers.append({
@@ -19,9 +18,7 @@ def configurations():
             "password": entry["password"],
             "platform": "ios"
         })
-
     saved_files = []
-
     for router in routers:
         try:
             router_conn = driver(hostname=router["hostname"],
@@ -31,20 +28,14 @@ def configurations():
             print(f"Connecting to {router['hostname']}")
             output = router_conn.get_config()["running"]
             router_conn.close()
-
             timestamp = datetime.utcnow().strftime('%Y-%m-%d T %H-%M-%S')
             filename = f"{router['Device']}:{timestamp}.txt"
-
             with open(filename, "w") as file:
                 file.write(output)
-
             print(f"Config file is saved as: {filename}")
             saved_files.append(filename)
-
         except Exception as e:
             print(f"Failed to get config: {e}")
-
     return saved_files
-
 if __name__ == "__main__":
     configurations()
